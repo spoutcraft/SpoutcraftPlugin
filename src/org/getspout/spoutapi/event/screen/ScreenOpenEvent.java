ffe6dbcf8436c4c41a83f8f04cda5a2189b8c88f
Top Cat <TopCat@thegigcast.net>
2011-08-13 23:05:20 +0100

    Call inventory open using screen events

--- src/org/getspout/spoutapi/event/screen/ScreenOpenEvent.java
+++ src/org/getspout/spoutapi/event/screen/ScreenOpenEvent.java
@@ -1,5 +1,9 @@
 package org.getspout.spoutapi.event.screen;
 
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.getspout.spout.SpoutNetServerHandler;
+import org.getspout.spoutapi.event.inventory.InventoryOpenEvent;
 import org.getspout.spoutapi.gui.Screen;
 import org.getspout.spoutapi.gui.ScreenType;
 import org.getspout.spoutapi.player.SpoutPlayer;
@@ -7,5 +11,13 @@ import org.getspout.spoutapi.player.SpoutPlayer;
 public class ScreenOpenEvent extends ScreenEvent{
 	public ScreenOpenEvent(SpoutPlayer player, Screen screen, ScreenType type) {
 		super("ScreenOpenEvent", player, screen, type);
+		CraftPlayer cp = (CraftPlayer) player;
+		SpoutNetServerHandler snsh = (SpoutNetServerHandler) cp.getHandle().netServerHandler;
+		if (type == ScreenType.PLAYER_INVENTORY && !snsh.activeInventory) {
+			snsh.activeInventory = true;
+			InventoryOpenEvent event = new InventoryOpenEvent(player, player.getInventory(), snsh.getDefaultInventory(), snsh.getActiveInventoryLocation());
+			Bukkit.getServer().getPluginManager().callEvent(event);
+			this.setCancelled(event.isCancelled());
+		}
 	}
 }
