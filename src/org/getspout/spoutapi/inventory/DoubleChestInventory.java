491a3442ef23e14358ebd00b5b2de0ccc41ab52d
Majiir <henry@zephyrhosting.net>
2011-08-14 01:07:04 -0400

    (Re)added accessors for left/right sides and direction of double chest.

--- src/org/getspout/spoutapi/inventory/DoubleChestInventory.java
+++ src/org/getspout/spoutapi/inventory/DoubleChestInventory.java
@@ -1,6 +1,7 @@
 package org.getspout.spoutapi.inventory;
 
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.inventory.Inventory;
 
 public interface DoubleChestInventory extends Inventory{
@@ -15,5 +16,22 @@ public interface DoubleChestInventory extends Inventory{
 	 * @return bottom half
 	 */
 	public Block getBottomHalf();
-
+	
+	/**
+	 * Gets the left side of the double chest
+	 * @return left half
+	 */
+	public Block getLeftSide();
+	
+	/**
+	 * Gets the right side of the double chest
+	 * @return right half
+	 */
+	public Block getRightSide();
+	
+	/**
+	 * Gets the direction of the front buckle on the double chest
+	 * @return buckle direction
+	 */
+	public BlockFace getDirection();
 }
