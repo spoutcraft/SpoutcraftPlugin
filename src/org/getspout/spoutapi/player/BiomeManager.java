5ac75f7e9af1001acd1818b963b3589786c735b7
Dalton Pelc <djpelc@mtu.edu>
2011-08-15 01:48:31 -0400

    Added javadocs

--- src/org/getspout/spoutapi/player/BiomeManager.java
+++ src/org/getspout/spoutapi/player/BiomeManager.java
@@ -5,12 +5,36 @@ import org.getspout.spoutapi.block.SpoutWeather;
 
 public interface BiomeManager {
 	
+	/**
+	 * Sets a players weather in a specific Biome to render as the specified SpoutWeather
+	 * 
+	 * @param player to set the biomes weather for
+	 * @param biome that gets set to the weather for the player
+	 * @param weather to set the biome to
+	 */
 	public void setPlayerBiomeWeather(SpoutPlayer player, Biome biome, SpoutWeather weather);
 	
+	/**
+	 * Sets a players weather in all biomes to render as the specified SpoutWeather
+	 * 
+	 * @param player to set the worlds weather for
+	 * @param weather to set the world to
+	 */
 	public void setPlayerWeather(SpoutPlayer player, SpoutWeather weather);
 	
+	/**
+	 * Sets the whole servers weather in a specific Biome to render as the specified SpoutWeather
+	 * 
+	 * @param biome that gets set to the weather for the world
+	 * @param weather to set the biome to
+	 */
 	public void setGlobalBiomeWeather(Biome biome, SpoutWeather weather);
 	
+	/** 
+	 * Sets the whole servers weather to render as the specified SpoutWeather
+	 * 
+	 * @param weather to set the world to
+	 */
 	public void setGlobalWeather(SpoutWeather weather);
 
 }
