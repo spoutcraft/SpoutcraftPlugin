6e6673f39a47b7eade261640fe8e2b3401237ec0
Dalton Pelc <djpelc@mtu.edu>
2011-08-15 01:12:03 -0400

    finishing up API

--- src/org/getspout/spoutapi/player/BiomeManager.java
+++ src/org/getspout/spoutapi/player/BiomeManager.java
@@ -1,9 +1,16 @@
 package org.getspout.spoutapi.player;
 
 import org.bukkit.block.Biome;
+import org.getspout.spoutapi.block.SpoutWeather;
 
 public interface BiomeManager {
 	
-	public void setPlayerBiomeWeather(SpoutPlayer player, Biome biome, byte weather);
+	public void setPlayerBiomeWeather(SpoutPlayer player, Biome biome, SpoutWeather weather);
+	
+	public void setPlayerWeather(SpoutPlayer player, SpoutWeather weather);
+	
+	public void setGlobalBiomeWeather(Biome biome, SpoutWeather weather);
+	
+	public void setGlobalWeather(SpoutWeather weather);
 
 }
