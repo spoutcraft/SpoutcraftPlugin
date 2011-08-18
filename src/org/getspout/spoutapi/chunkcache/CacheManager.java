34cc503bddfd11a87212ed0df4d1b6fa494b5795
raphfrk <raphfrk@gmail.com>
2011-08-18 19:30:15 +0100

    Resends chunk data when client cache corrupted

--- src/org/getspout/spoutapi/chunkcache/CacheManager.java
+++ src/org/getspout/spoutapi/chunkcache/CacheManager.java
@@ -9,9 +9,16 @@ public interface CacheManager {
 	 * @param id the entity id of the player
 	 * @param add true if the hashes are to be added to the pool
 	 * @param hashesh array of hashes to be added/removed from the hash pool
-	 * @return an empty MCPacket of type packetId
 	 * 
 	 */
 	void handle(int id, boolean add, long[] hashes);
 	
+	/**
+	 * Requests that a chunk is refreshed
+	 * 
+	 * @param id the player's id
+	 * @param cx the chunk X coord
+	 * @param cz the chunk Y coord
+	 */
+	void refreshChunkRequest(int id, int cx, int cz);
 }
