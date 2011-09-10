c91441c0c7d1918118846598294a00364e6b0fdb
Dalton Pelc <djpelc@mtu.edu>
2011-09-10 01:47:13 -0400

    organized items a bit

---
+++ src/org/getspout/spoutapi/material/item/GenericArmor.java
@@ -0,0 +1,11 @@
+package org.getspout.spoutapi.material.item;
+
+import org.getspout.spoutapi.material.Armor;
+
+public class GenericArmor extends GenericItem implements Armor {
+
+	public GenericArmor(int id) {
+		super(id);
+	}
+
+}
---
+++ src/org/getspout/spoutapi/material/item/GenericWeapon.java
@@ -0,0 +1,11 @@
+package org.getspout.spoutapi.material.item;
+
+import org.getspout.spoutapi.material.Weapon;
+
+public class GenericWeapon extends GenericItem implements Weapon {
+
+	public GenericWeapon(int id) {
+		super(id);
+	}
+
+}
