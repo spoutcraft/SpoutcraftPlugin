926d11729f0caee1a6d4c0c32aaa195f84ad9d7b
narrowtux <narrow.m@gmail.com>
2011-09-16 20:44:38 +0200

    Implemented Global GUI

--- src/org/getspout/spoutapi/gui/GenericOverlayScreen.java
+++ src/org/getspout/spoutapi/gui/GenericOverlayScreen.java
@@ -1,38 +1,14 @@
 package org.getspout.spoutapi.gui;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
-
 public class GenericOverlayScreen extends GenericScreen implements OverlayScreen {
 
-	int entityId;
 	ScreenType screenType;
 	
 	public GenericOverlayScreen(int entityId, ScreenType type) {
-		System.out.println("Created OverlayScreen of "+type);
+		super(entityId);
 		screenType = type;
-		this.entityId = entityId;
 	}
-
-	@Override
-	public int getNumBytes() {
-		return super.getNumBytes() + 4;
-	}
-
-	@Override
-	public void readData(DataInputStream input) throws IOException {
-		super.readData(input);
-		screenType = ScreenType.getType(input.readInt());
-	}
-
-	@Override
-	public void writeData(DataOutputStream output) throws IOException {
-		super.writeData(output);
-		output.write(screenType.getCode());
-	}
-
+	
 	@Override
 	public WidgetType getType() {
 		return WidgetType.OverlayScreen;
