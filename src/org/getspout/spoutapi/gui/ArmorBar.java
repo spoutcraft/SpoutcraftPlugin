56d782ebb4b4ab4836ffd9fe5e615f7c246d3786
Olloth <olloth@gmail.com>
2011-09-20 07:44:27 -0400

    Derp missed ArmorBar. Only my 30th commit on the HUD

--- src/org/getspout/spoutapi/gui/ArmorBar.java
+++ src/org/getspout/spoutapi/gui/ArmorBar.java
@@ -28,7 +28,7 @@ public class ArmorBar extends GenericWidget{
 
 	public ArmorBar() {
 		setDirty(false);
-		setX(427 / 2 + 82); //295
+		setX(427 / 2 - 91); //122
 		setY(191);
 		setAnchor(WidgetAnchor.BOTTOM_CENTER);
 	}
