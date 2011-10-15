b56781095108bd0cd1d244f7776ec67c1f1b71f4
Skyler <skyler.welch91@gmail.com>
2011-10-15 14:59:44 +0100

    Separating visual cheats

--- src/org/getspout/spoutapi/packet/PacketAllowVisualCheats.java
+++ src/org/getspout/spoutapi/packet/PacketAllowVisualCheats.java
@@ -20,27 +20,27 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-public class PacketAllowVisualCheats implements SpoutPacket{
-	private boolean cheating = false;
-        private boolean sky = false;
-        private boolean clearwater = false;
-        private boolean cloudheight = false;
-        private boolean stars = false;
-        private boolean weather = false;
-        private boolean time = false;
+public class PacketAllowVisualCheats implements SpoutPacket {
+    private boolean sky = false;
+    private boolean clearwater = false;
+    private boolean cloudheight = false;
+    private boolean stars = false;
+    private boolean weather = false;
+    private boolean time = false;
+    private boolean coords = false;
         
 	public PacketAllowVisualCheats() {
 
 	}
 	
-	public PacketAllowVisualCheats(boolean allow, boolean sky, boolean clearwater, boolean cloudheight, boolean stars, boolean weather, boolean time) {
-		this.cheating = allow;
-                this.sky = sky;
-                this.clearwater = clearwater;
-                this.cloudheight = cloudheight;
-                this.stars = stars;
-                this.weather = weather;
-                this.time = time;
+	public PacketAllowVisualCheats(boolean tsky, boolean tclearwater, boolean tcloudheight, boolean tstars, boolean tweather, boolean ttime, boolean tcoords) {
+        this.sky = tsky;
+        this.clearwater = tclearwater;
+        this.cloudheight = tcloudheight;
+        this.stars = tstars;
+        this.weather = tweather;
+        this.time = ttime;
+        this.coords = tcoords;
 	}
 
 	@Override
@@ -50,24 +50,24 @@ public class PacketAllowVisualCheats implements SpoutPacket{
 
 	@Override
 	public void readData(DataInputStream input) throws IOException {
-		cheating = input.readBoolean();
-                sky = input.readBoolean();
-                clearwater = input.readBoolean();
-                cloudheight = input.readBoolean();
-                stars = input.readBoolean();
-                weather = input.readBoolean();
-                time = input.readBoolean();                
+            sky = input.readBoolean();
+            clearwater = input.readBoolean();
+            cloudheight = input.readBoolean();
+            stars = input.readBoolean();
+            weather = input.readBoolean();
+            time = input.readBoolean();     
+            coords = input.readBoolean();
 	}
 
 	@Override
 	public void writeData(DataOutputStream output) throws IOException {
-		output.writeBoolean(cheating);
-                output.writeBoolean(sky);
-                output.writeBoolean(clearwater);
-                output.writeBoolean(cloudheight);
-                output.writeBoolean(stars);
-                output.writeBoolean(weather);
-                output.writeBoolean(time);
+            output.writeBoolean(sky);
+            output.writeBoolean(clearwater);
+            output.writeBoolean(cloudheight);
+            output.writeBoolean(stars);
+            output.writeBoolean(weather);
+            output.writeBoolean(time);
+            output.writeBoolean(coords);
 	}
 
 	@Override
