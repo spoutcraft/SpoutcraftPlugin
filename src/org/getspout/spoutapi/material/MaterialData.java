eb81402a6b0ba43c16b13af3a68727eed1b02298
Olloth <olloth@gmail.com>
2011-10-16 20:44:25 -0400

    Forgot StoneBricks in MaterialData

--- src/org/getspout/spoutapi/material/MaterialData.java
+++ src/org/getspout/spoutapi/material/MaterialData.java
@@ -147,7 +147,9 @@ public class MaterialData {
 	public static final Block lockedChest = new Solid("Locked Chest", 95);
 	public static final Block trapdoor = new Solid("Trapdoor", 96);
 	public static final Block silverfishStone = new Solid("Silverfish Stone", 97);
-	public static final Block stoneBricks = new Solid("Stone Brick", 98);
+	public static final Block stoneBricks = new StoneBricks("Stone Brick", 98, 0);
+	public static final Block mossyStoneBricks = new StoneBricks("Mossy Stone Brick", 98, 1);
+	public static final Block crackedStoneBricks = new StoneBricks("Cracked Stone Brick", 98, 2);
 	public static final Block hugeRedMushroom = new Solid("Huge Red Mushroom", 99);
 	public static final Block hugeBrownMushroom = new Solid("Huge Brown Mushroom", 100);
 	public static final Block ironBars = new Solid("Iron Bars", 101);
