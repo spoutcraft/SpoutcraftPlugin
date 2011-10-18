55b3cdf54d94c7f67c9f8b8b7146c3523ef7c411
Afforess <afforess@gmail.com>
2011-10-18 13:02:00 -0400

    Reversed mushroom id's

--- src/org/getspout/spoutapi/material/MaterialData.java
+++ src/org/getspout/spoutapi/material/MaterialData.java
@@ -151,8 +151,8 @@ public class MaterialData {
 	public static final Block stoneBricks = new StoneBricks("Stone Brick", 98, 0);
 	public static final Block mossyStoneBricks = new StoneBricks("Mossy Stone Brick", 98, 1);
 	public static final Block crackedStoneBricks = new StoneBricks("Cracked Stone Brick", 98, 2);
-	public static final Block hugeRedMushroom = new Solid("Huge Red Mushroom", 99);
-	public static final Block hugeBrownMushroom = new Solid("Huge Brown Mushroom", 100);
+	public static final Block hugeBrownMushroom = new Solid("Huge Brown Mushroom", 99);
+	public static final Block hugeRedMushroom = new Solid("Huge Red Mushroom", 100);
 	public static final Block ironBars = new Solid("Iron Bars", 101);
 	public static final Block glassPane = new Solid("Glass Pane", 102);
 	public static final Block watermelon = new Solid("Watermelon", 103);
