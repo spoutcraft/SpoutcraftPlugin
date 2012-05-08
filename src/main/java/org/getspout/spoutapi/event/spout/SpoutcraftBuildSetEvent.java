07db2d68951c18cad0aa78fd66bda67b44f3cff1
Steven Downer <NinjaGrinch@hotmail.com>
2012-05-08 01:01:42 -0400

    Added SpoutcraftBuildSetEvent with a huge thanks to NinjaZidane Signed-off-by: Steven Downer <NinjaGrinch@hotmail.com>

---
+++ src/main/java/org/getspout/spoutapi/event/spout/SpoutcraftBuildSetEvent.java
@@ -0,0 +1,71 @@
+/*
+ * This file is part of SpoutPluginAPI (http://www.spout.org/).
+ *
+ * SpoutPluginAPI is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SpoutPluginAPI is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.getspout.spoutapi.event.spout;
+
+import org.getspout.spoutapi.player.SpoutPlayer;
+
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class SpoutcraftBuildSetEvent extends Event implements Cancellable {
+	private static final HandlerList handlers = new HandlerList();
+	private final SpoutPlayer player;
+	private final int build;
+
+	private boolean isCancelled = false;
+
+	public SpoutcraftBuildSetEvent(SpoutPlayer s, int build) {
+		player = s;
+		this.build = build;
+	}
+
+	/**
+	 * Gets the player associated with this event.
+	 * @return
+	 */
+	public SpoutPlayer getPlayer() {
+		return player;
+	}
+
+	/**
+	 * Gets the build associated with this event.
+	 * @return
+	 */
+	public int getBuild() {
+		return build;
+	}
+
+	@Override
+	public HandlerList getHandlers() {
+		return handlers;
+	}
+
+	public static HandlerList getHandlerList() {
+		return handlers;
+	}
+
+	@Override
+	public boolean isCancelled() {
+		return isCancelled;
+	}
+
+	@Override
+	public void setCancelled(boolean b) {
+		isCancelled = b;
+	}
+}
