3e03597df6397061a37bb77785b736271b55f7d2
Afforess <afforess@gmail.com>
2012-06-29 19:17:30 -0300

    prevent exceptions when removing widgets from spout

--- src/main/java/org/getspout/spoutapi/gui/GenericScreen.java
+++ src/main/java/org/getspout/spoutapi/gui/GenericScreen.java
@@ -100,9 +100,11 @@ public abstract class GenericScreen extends GenericWidget implements Screen {
 
 	@Override
 	public Screen removeWidgets(Plugin p) {
-		for (Widget i : getAttachedWidgets()) {
-			if (widgets.get(i) != null && widgets.get(i).equals(p)) {
-				removeWidget(i);
+		if (p != Bukkit.getServer().getPluginManager().getPlugin("Spout")) {
+			for (Widget i : getAttachedWidgets()) {
+				if (widgets.get(i) != null && widgets.get(i).equals(p)) {
+					removeWidget(i);
+				}
 			}
 		}
 		return this;
