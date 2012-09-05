0262b0b222e936e735bf76586fb056c25946bdd8
Zloteanu Nikita <teapa011@gmail.com>
2012-09-05 05:42:04 -0700

    Experimental optimization to custom chunk sending, breaks old SpoutPlugin versions w/ new Spoutcraft!
    Signed-off by Zloteanu Nikita<teapa011@gmail.com>

--- src/main/java/org/getspout/spoutapi/packet/PacketType.java
+++ src/main/java/org/getspout/spoutapi/packet/PacketType.java
@@ -89,7 +89,8 @@ public enum PacketType {
 	PacketSlotClick(59, PacketSlotClick.class),
 	PacketWaypoint(60, PacketWaypoint.class),
 	PacketParticle(61, PacketParticle.class),
-	PacketAccessory(62, PacketAccessory.class);
+	PacketAccessory(62, PacketAccessory.class),
+	PacketCustomChunks(63, PacketCustomChunks.class);
 	private final int id;
 	private final Class<? extends SpoutPacket> packetClass;
 	private static final HashMap<Integer, PacketType> lookupId = new HashMap<Integer, PacketType>();
