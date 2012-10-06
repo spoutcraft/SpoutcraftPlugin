bc76a1c83256eab646bfb257ae61276d1d23d5ad
Andre Mohren <andre@eiveo.net>
2012-10-06 05:50:16 +0200

    Implemented custom-block collision rotation.
    Signed-off-by: Andre Mohren <andre@eiveo.net>

--- src/main/java/org/getspout/spout/SpoutPlayerListener.java
+++ src/main/java/org/getspout/spout/SpoutPlayerListener.java
@@ -205,6 +205,18 @@ public class SpoutPlayerListener implements Listener {
 									}
 								}
 								player.updateInventory();
+								// Now we have placed a nice custom block! We should check its rotation and rotate the base block!
+								if (cb.canRotate()) {
+									if (rot == 1) {
+										block.setData((byte) 1);
+									} else if (rot == 2) {
+										block.setData((byte) 2);
+									} else if (rot == 3) {
+										block.setData((byte) 0);
+									} else {
+										block.setData((byte) 3);
+									}
+								}
 							} else {
 								// Event cancelled or can't build
 								mm.removeBlockOverride(block);
