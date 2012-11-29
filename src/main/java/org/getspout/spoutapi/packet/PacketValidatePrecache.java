2d4441a605b9c1b9b2f5edd11d02efd9caa3532f
Chris Sanders <zidane@outlook.com>
2012-11-29 17:12:34 -0600

    Cleanup. Signed-off-by: Chris Sanders <zidane@outlook.com>

--- src/main/java/org/getspout/spoutapi/packet/PacketValidatePrecache.java
+++ src/main/java/org/getspout/spoutapi/packet/PacketValidatePrecache.java
@@ -4,42 +4,37 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map.Entry;
 
-import org.bukkit.Bukkit;
-import org.bukkit.plugin.Plugin;
 import org.getspout.spout.precache.PrecacheTuple;
 import org.getspout.spoutapi.io.SpoutInputStream;
 import org.getspout.spoutapi.io.SpoutOutputStream;
 
+import org.bukkit.plugin.Plugin;
+
 public class PacketValidatePrecache implements SpoutPacket {
-	
 	int count;
 	PrecacheTuple[] plugins;
-	
-	public PacketValidatePrecache() {
-	}
-	
+
 	public PacketValidatePrecache(HashMap<Plugin, Long> pluginMap) {
 		count = pluginMap.size();
 		plugins = new PrecacheTuple[count];
 		int i = 0;
 		for (Entry entry : pluginMap.entrySet()) {
-			Plugin p = (Plugin)entry.getKey();
+			Plugin p = (Plugin) entry.getKey();
 			plugins[i] = new PrecacheTuple(
 					p.getDescription().getName(),
 					p.getDescription().getVersion(),
-					(Long)entry.getValue()
-				);
+					(Long) entry.getValue()
+			);
 			i++;
 		}
 	}
-	
-	
+
 	@Override
 	public void readData(SpoutInputStream input) throws IOException {
 		count = input.readInt();
-		plugins = new PrecacheTuple[count];
 		if (count > 0) {
-			for(int i = 0; i<count; i++) {
+			plugins = new PrecacheTuple[count];
+			for (int i = 0; i < count; i++) {
 				String plugin = input.readString();
 				String version = input.readString();
 				long crc = input.readLong();
@@ -51,7 +46,7 @@ public class PacketValidatePrecache implements SpoutPacket {
 	@Override
 	public void writeData(SpoutOutputStream output) throws IOException {
 		output.writeInt(count);
-		for (int i=0; i<count; i++) {
+		for (int i = 0; i < count; i++) {
 			output.writeString(plugins[i].getPlugin());
 			output.writeString(plugins[i].getVersion());
 			output.writeLong(plugins[i].getCrc());
@@ -64,7 +59,7 @@ public class PacketValidatePrecache implements SpoutPacket {
 
 	@Override
 	public void failure(int playerId) {
-		
+
 	}
 
 	@Override
@@ -76,5 +71,4 @@ public class PacketValidatePrecache implements SpoutPacket {
 	public int getVersion() {
 		return 0;
 	}
-
 }
