80a24495fe6f22a9b6801a6fc7c6e521ffcb9e2b
Luke Spragg <luke@lukespragg.com>
2013-10-21 17:26:31 -0400

    Remove duplicate header. PS: license did not change.

--- src/main/java/org/getspout/spoutapi/inventory/MaterialManager.java
+++ src/main/java/org/getspout/spoutapi/inventory/MaterialManager.java
@@ -17,25 +17,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * This file is part of SpoutPlugin.
- *
- * Copyright (c) 2011 Spout LLC <http://www.spout.org/>
- * SpoutPlugin is licensed under the GNU Lesser General Public License.
- *
- * SpoutPlugin is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SpoutPlugin is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 package org.getspout.spoutapi.inventory;
 
 import java.util.Set;
